d54ad8bc31423885ab332bab4d8f62d91ef35115
k8ieone@pinephone <rostik.medved@gmail.com>
2022-08-23 17:57:37 +0100

    Restructured to prepare for actually making this into something

--- mphandy/window.py
+++ mphandy/window.py
@@ -12,7 +12,7 @@ class MainWindow(Gtk.ApplicationWindow):
         self.build_main_view(self)
         GLib.set_application_name("mphandy")
 
-    def build_main_view(self, *args, **kwargs):
+    def build_stack_switcher(self):
         # Defining stack and stackswitcher
         self.stack = Gtk.Stack()
         self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
@@ -30,61 +30,87 @@ class MainWindow(Gtk.ApplicationWindow):
         # Set the stack switcher box as the title widget
         self.header.set_title_widget(self.swboxh)
 
+    def build_root_box(self):
         # Box for all the boxes
         self.rootbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
         # root box to window
         self.set_child(self.rootbox)
-
         # Add the stack to the root box
         self.rootbox.append(self.stack)
 
+    def build_player_box(self):
         # Box with all the buttons
-        self.buttonbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.buttonbox.set_margin_top(20)
-        self.buttonbox.set_margin_bottom(20)
-        self.buttonbox.set_margin_start(20)
-        self.buttonbox.set_margin_end(20)
-        self.buttonbox.set_spacing(100)
-        self.buttonbox.set_homogeneous(True)
-        self.button = Gtk.Button(label="Hello there!")
-        self.button.connect('clicked', self.hello)
-        self.button2 = Gtk.Button(label="MPD")
-        self.button2.connect('clicked', self.list_mpd_root)
-        self.buttonbox.append(self.button) # Put button in the first of the two vertial boxes
-        self.buttonbox.append(self.button2) # Put button in the first of the two vertial boxes
+        buttonbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        buttonbox.set_margin_top(20)
+        buttonbox.set_margin_bottom(20)
+        buttonbox.set_margin_start(20)
+        buttonbox.set_margin_end(20)
+        buttonbox.set_spacing(100)
+        buttonbox.set_homogeneous(True)
+        button = Gtk.Button(label="Hello there!")
+        button.connect('clicked', self.hello)
+        button2 = Gtk.Button(label="MPD")
+        button2.connect('clicked', self.list_mpd_root)
+        buttonbox.append(button) # Put button in the first of the two vertial boxes
+        buttonbox.append(button2) # Put button in the first of the two vertial boxes
+        self.stack.add_titled(buttonbox, "1", "Now playing")
 
-        self.check = Gtk.CheckButton(label="And goodbye?")
+    def build_browser_box(self):
+        # Box with all the buttons
+        browserbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        browserbox.set_margin_top(20)
+        browserbox.set_margin_bottom(20)
+        browserbox.set_margin_start(20)
+        browserbox.set_margin_end(20)
+        browserbox.set_spacing(100)
+        browserbox.set_homogeneous(True)
+        button = Gtk.Button(label="Hello there!")
+        button.connect('clicked', self.hello)
+        button2 = Gtk.Button(label="MPD")
+        button2.connect('clicked', self.list_mpd_root)
+        browserbox.append(button) # Put button in the first of the two vertial boxes
+        browserbox.append(button2) # Put button in the first of the two vertial boxes
+        self.stack.add_titled(browserbox, "2", "Browse")
 
-        # Add the button box to the stack page
-        self.stack.add_titled(self.buttonbox, "1", "Now playing")
-        self.stack.add_titled(self.check, "2", "Browse")
-
+    def build_hamburger_menu(self):
         # Create a new action
-        action = Gio.SimpleAction.new("about", None)
-        action.connect("activate", self.show_about)
-        self.add_action(action)
+        about_action = Gio.SimpleAction.new("about", None)
+        about_action.connect("activate", self.show_about)
+        self.add_action(about_action)
 
         menu = Gio.Menu.new()
         menu.append("About", "win.about")
 
-        self.popover = Gtk.PopoverMenu()
-        self.popover.set_menu_model(menu)
+        popover = Gtk.PopoverMenu()
+        popover.set_menu_model(menu)
 
         self.hamburger = Gtk.MenuButton()
-        self.hamburger.set_popover(self.popover)
+        self.hamburger.set_popover(popover)
         self.hamburger.set_icon_name("open-menu-symbolic")
         self.header.pack_end(self.hamburger)
+
+    def build_main_view(self, *args, **kwargs):
+        # These functions build the contents of the main window
+        self.build_stack_switcher()
+        self.build_root_box()
+        self.build_player_box()
+        self.build_browser_box()
+        self.build_hamburger_menu()
 
     def hello(self, button):
         print("General Kenobi...")
 
     def list_mpd_root(self, button):
         self.dialog = Gtk.MessageDialog(text="Server's response", secondary_text=client.client_test())
+        self.dialog.add_button("Ok", 1)
+        self.dialog.connect("response", self.close_dialog)
         self.dialog.set_transient_for(self)
         self.dialog.set_modal(self)
         self.dialog.present()
         print(client.client_test())
+
+    def close_dialog(self, action, param):
+        self.dialog.close()
 
     def show_about(self, action, param):
         self.about = Gtk.AboutDialog()
